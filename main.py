5a35d09bbc0a8424d6fc7527b32e13c9e720bd19
madishak <33802709+madishak@users.noreply.github.com>
2019-09-04 17:59:35 +0300

    added file

--- main.py
+++ main.py
@@ -1,60 +1,115 @@
-class Test:
-    def __init__(self, nums):
-        self.nums = nums
-        self.storage = []
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from plyer import notification
+from datetime import datetime, date, time
+import os
 
-    def increase(self):
-        self.storage.append(self.nums)
-        flag = True
-        while (flag):
-            flag = False
-            for i in range(len(self.nums)):
-                if self.nums[i] > self.nums[i+1]:
-                    temp = self.nums[i]
-                    self.nums[i] = self.nums[i+1]
-                    self.nums[i+1] = temp
-                    flag = True
-                    return self.nums
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-    def printStorage(self):
-        return self.storage
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'messages.db')
+db = SQLAlchemy(app)
+
+
+class Login(db.Model):
+    id = db.Column(db.Integer,  primary_key=True)
+    name = db.Column(db.String(1024), nullable=False)
+    password = db.Column(db.String(1024), nullable=False)
+    messages = db.relationship('Message', backref='user', lazy='dynamic')
+
+    # def __init__(self, name, password):
+    #     self.name = name.strip()
+    #     self.password = password.strip()
+    #     # self.text = [
+    #     #     Message(text=t.strip()) for t in text.split(',')
+    #     # ]
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    receiver = db.Column(db.String(1024), nullable=False)
+    text = db.Column(db.String(32), nullable=False)
+    # date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('login.id'))
+
+
+
+    # def __init__(self, text):
+    #     self.text = text.strip()
+
+    def __repr__(self):
+        return '<Message %r>' % self.text
 
 
 
 
+db.create_all()
 
-t = Test([9,3,6,7,1,7])
+current_user = {'user_id': 0}
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html', login=Login.query.all())
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
+@app.route('/get_user', methods=['POST'])
+def get_user():
+    name = request.form['log__name']
+    password = request.form['password']
+    user = Login(name=name, password=password)
+    db.session.add(user)
+    db.session.commit()
+    current_user['user_id'] = user.id
+    return redirect(url_for('main', user_id=current_user['user_id']))
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
 
-print(t.increase())
-print(t.printStorage())
-print("======================")
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', result=db.session.query(Login.name, Message.receiver, Message.text).filter(Login.id == Message.user_id).all(), login=Login.query.all())
 
+
+@app.route('/login', methods=['GET'])
+def get_names():
+    return render_template('main.html', login=Login.query.all())
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    print(current_user['user_id'])
+    print("Madina!!!!!!!!!!!!!!!!!!")
+    # name = Login.name
+
+    receiver = request.form.get('receiver')
+    text = request.form['message']
+    print(receiver)
+
+    users = Login.query.get(current_user['user_id'])
+    message = Message(receiver="receiver", text=text, user=users)
+
+    # r = Message(receiver=receiver, text=text, user=users, date=datetime.datetime.utcnow(tz=None))
+
+    if len(text) == 0:
+        notification.notify(
+            title='Ошибка',
+            message='Введите сообщение'
+        )
+
+
+    db.session.add(message)
+    db.session.commit()
+
+    return redirect(url_for('main'))
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '127.0.0.1', port = 5000)
